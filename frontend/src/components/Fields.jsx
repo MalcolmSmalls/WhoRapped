da41c85a384df27195690c54fba5728329d2978e
MalcolmSmalls <smallsbeats@gmail.com>
2023-02-24 08:49:41 -0800

    Add more options

--- frontend/src/components/Fields.jsx
+++ frontend/src/components/Fields.jsx
@@ -2,13 +2,54 @@ import React from 'react'
 
 export default function Fields() {
   return (
-    <div>
-      <input type='radio' id='easy' />
-      <label htmlFor='easy'>Easy</label>
-      <input type='radio' id='medium' />
-      <label htmlFor='medium'>Medium</label>
-      <input type='radio' id='hard' />
-      <label htmlFor='hard'>Hard</label>
+    <div className='container flex gap-10'>
+      <div>
+        <ul>
+          <li>
+            <input type='radio' id='easy' name='difficulty' className='mr-1' />
+            <label htmlFor='easy'>Easy</label>
+          </li>
+          <li>
+            <input
+              type='radio'
+              id='medium'
+              name='difficulty'
+              className='mr-1'
+            />
+            <label htmlFor='medium'>Medium</label>
+          </li>
+          <li>
+            <input type='radio' id='hard' name='difficulty' className='mr-1' />
+            <label htmlFor='hard'>Hard</label>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <input
+              type='radio'
+              id='underground'
+              name='underground'
+              className='mr-1'
+            />
+            <label htmlFor='underground'>Underground</label>
+          </li>
+          <li>
+            <input type='radio' id='indie' name='indie' className='mr-1' />
+            <label htmlFor='indie'>Indie</label>
+          </li>
+          <li>
+            <input
+              type='radio'
+              id='mainstream'
+              name='mainstream'
+              className='mr-1'
+            />
+            <label htmlFor='mainstream'>Mainstream</label>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
