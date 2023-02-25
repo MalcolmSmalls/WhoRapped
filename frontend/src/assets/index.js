4dbb2f1495da69fdb9f99a80d70a64529b47c276
MalcolmSmalls <smallsbeats@gmail.com>
2023-02-25 14:41:24 -0800

    Finish warning modal

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,5 +1,6 @@
 import nas from './nas.png'
 import jayz from './jayz.png'
 import kanye from './kanye.png'
+import pa_warning from './pa_warning.svg'
 
-export { nas, jayz, kanye }
+export { nas, jayz, kanye, pa_warning }
--- frontend/src/components/Warning.jsx
+++ frontend/src/components/Warning.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Main from './Main'
+import { pa_warning } from '../assets'
 
 export default function Warning() {
   return (
-    <div>
-      <h1>Warning</h1>
-      <Link to={'main'}>Let's Gooo!</Link>
-      <Link to='http://google.com'>No Way</Link>
+    <div className='w-screen flex h-screen items-center justify-center  font-Poppins'>
+      <div className='container flex h-64 justify-center mx-auto items-center rounded-xl shadow-xl gap-5 max-w-lg p-10 space-y-8'>
+        <img className='w-28' src={pa_warning} />
+        <div>
+          <h1 className='text-xl font-bold'>Warning</h1>
+          <p className='text-slate-500 text-sm '>
+            As the rawest musical genre formed, said lyrics may contain language
+            deemed inappropriate for younger audiences. Only click 'okay' if
+            you're sure, and young ones scatter like a glass jaw, and break it.
+          </p>
+          <div className='container flex gap-5 justify-center mt-5'>
+            <Link
+              to={'main'}
+              className='border border-slate-200  rounded-lg p-2 px-5'
+            >
+              Okay!
+            </Link>
+            <Link
+              to='http://google.com'
+              className='border border-slate-200  rounded-lg p-2 px-5'
+            >
+              Scatter!
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
